b9b3fa16653bbfc95fd7307d951018a17fa04fd3
nicholasjb89 <nicholasjbailey@hotmail.com>
2016-03-12 21:03:59 -0500

    Teaching over

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,13 +5,17 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    score_value = models.IntegerField(default=0)
+    def __str__(self):
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.question_text
 
 class myObj(models.Model):
     myVar = models.CharField(max_length=200)
+
 
