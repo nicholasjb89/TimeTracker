a82b010769585869bb19751c1b37ac66e0298326
nicholasjb89 <nicholasjbailey@hotmail.com>
2016-03-26 18:30:55 -0400

    Part 5 first test running
    Get some!

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,13 +9,16 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-        return self.question_text
+        return self.choice_text
 
 class myObj(models.Model):
     myVar = models.CharField(max_length=200, default="")
