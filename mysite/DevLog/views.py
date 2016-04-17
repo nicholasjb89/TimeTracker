817292d527b3ed0fe28b1cd4b9827a94252a1313
nicholasjb89 <nicholasjbailey@hotmail.com>
2016-04-16 20:37:08 -0400

    Good luck nick
    Love MaRc

--- mysite/DevLog/views.py
+++ mysite/DevLog/views.py
@@ -38,54 +38,35 @@ class ChartView(generic.ListView):
     def get_queryset(self):
         """Django SQL to get data."""
         logs = Log.objects.filter(category__name__contains="Development").order_by('date_time') # Get all Development logs, ordered by date_time
-            # hours = Log.objects.filter for hours by distinct date... but I don't remember how to do this shit
 
         # foreach log in logs,
         # get hours per unique day
-        # return { Date : Hours } to plot on graph Development
+        # return { Labels of Unique Dates & Series of Total Hours } to plot on the graph
 
+        # init()
         sameDay = logs[0].date_time.day # set to first day
         hoursInSameDay = 0.0;
-        graphPoints = {}
-        # dictionary[newKey] = value;
-        # 2,3,4,5,6
-
         labelList = []
         seriesList = []
 
-        #seperate our list of logs into labels and series for graphing Labels: Unique day, Series: Total Hours
+
+        # seperate our list of logs into labels and series for graphing
+        # Labels as Unique day, Series: Total Hours
         for log in logs:
             # TODO: Fix this so instead of June 2nd and May 2nd both returning 2 for the key, have them return unique dates for unique keys
             if sameDay == log.date_time.day:
                hoursInSameDay += log.hours
             else:
-                # graphPoints[sameDay] = hoursInSameDay
                 labelList.append(sameDay) # submit label
                 seriesList.append(hoursInSameDay) # submit series value
                 sameDay = log.date_time.day # get next day
                 hoursInSameDay = log.hours # reloop
 
-        #graphPoints[sameDay] = hoursInSameDay
         labelList.append(sameDay) # submit last unique day
         seriesList.append(hoursInSameDay) # submit last unique series value
         print(labelList)
         graphPoints = {"labels":labelList, "series":seriesList}
-        # myTubel = (labelList, seriesList)
-        # print(graphPoints)
         return graphPoints
-        # for log in logs:
-        #     # TODO: Fix this so instead of June 2nd and May 2nd both returning 2 for the key, have them return unique dates for unique keys
-        #     if sameDay == log.date_time.day:
-        #        hoursInSameDay += log.hours
-        #     else:
-        #         graphPoints[sameDay] = hoursInSameDay # submit
-        #         sameDay = log.date_time.day # get next day
-        #         hoursInSameDay = log.hours # reloop
-        #
-        # graphPoints[sameDay] = hoursInSameDay # submit last unique day
-        #
-        #
-        # return graphPoints #total hours on different days
 
 
 class CategoryGraphView(generic.DetailView):
